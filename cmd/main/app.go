209d9af336cd7e699f3e9169ab87f3a52812ac3f
Kekelberi <alexei.gaponov@gmail.com>
2022-07-04 10:55:53 +0300

    save

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/julienschmidt/httprouter"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	name := params.ByName("name")
+	w.Write([]byte(fmt.Sprintf("hello%s\n", name)))
+}
+
+func main() {
+	router := *httprouter.New()
+	router.GET("/", indexHandler)
+
+	listener, err := new.Listen("tcp", ":3000")
+	if err != nil {panic(err)}
+if err != nil {panic(err)}
 }
