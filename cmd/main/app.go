bb9113729a17e04ca6ad8e47d3935a441c73ee2f
Kekelberi <alexei.gaponov@gmail.com>
2022-07-04 11:29:35 +0300

    save

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -2,21 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
 	"net/http"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	name := params.ByName("name")
-	w.Write([]byte(fmt.Sprintf("hello%s\n", name)))
+	fmt.Fprint(w, "{id: 1, title: 2}")
+}
+
+func Hello(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", params.ByName("name"))
 }
 
 func main() {
-	router := *httprouter.New()
+	router := httprouter.New()
 	router.GET("/", indexHandler)
+	router.GET("/hello/:name", Hello)
 
-	listener, err := new.Listen("tcp", ":3000")
-	if err != nil {panic(err)}
-if err != nil {panic(err)}
+	listener, err := net.Listen("tcp", ":3000")
+	if err != nil {
+		panic(err)
+	}
+
+	server := &http.Server{
+		Handler:      router,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Fatal(server.Serve(listener))
 }
