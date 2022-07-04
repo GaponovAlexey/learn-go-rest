ae278716f70d49430d7161d3d8be7b7996d03231
Kekelberi <alexei.gaponov@gmail.com>
2022-07-04 11:53:07 +0300

    save

--- cmd/api/index.go
+++ cmd/api/index.go
@@ -23,7 +23,7 @@ func main() {
 	router.GET("/", indexHandler)
 	router.GET("/hello/:name", Hello)
 
-	listener, err := net.Listen("tcp", ":3000")
+	listener, err := net.Listen("tcp", "learn-go-rest.vercel.app")
 	if err != nil {
 		panic(err)
 	}
